d15c5b9c4fe8271cfcc2545e9f4dfe41350996f0
Caiwenxuan <602046764@qq.com>
2024-02-22 21:57:48 +0800

    feat: add blog exchange with JSON

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,32 +1,45 @@
 export function isJSON(str: string): boolean {
-  if (typeof str == 'string') {
-      try {
-          var obj=JSON.parse(str);
-          if(typeof obj == 'object' && obj ){
-              return true;
-          }else{
-              return false;
-          }
-
-      } catch(e) {
-          console.log('error：'+str+'!!!'+e);
-          return false;
+  if (typeof str == "string") {
+    try {
+      var obj = JSON.parse(str);
+      if (typeof obj == "object" && obj) {
+        return true;
+      } else {
+        return false;
       }
+    } catch (e) {
+      console.log("error：" + str + "!!!" + e);
+      return false;
+    }
   }
-  return false
+  return false;
 }
-
 
 export const recordIsSong = (record: Song | Album | Artist): record is Song =>
-(record as Song).artists ? true : false;
+  (record as Song).artists ? true : false;
 export const recordIsAlbum = (record: Song | Album | Artist): record is Album =>
-(record as Album).artist ? true : false;
+  (record as Album).artist ? true : false;
 
-/**
- * @description Unit8字节流转JSON
- */
-export const unit8Array2JSON = (unit: Uint8Array[]) => {
-    return new Promise((resolve) => {
-        // resolve(JSON.parse(String.fromCharCode.apply(null, unit as)))
-    })
-}
+export const transBlob2JSON = (blob: Blob) => {
+  return new Promise((resolve, reject) => {
+    let read = new FileReader();
+
+    read.readAsText(blob, "utf-8");
+    read.onload = (data) => {
+      if (data && data.target) {
+        resolve(JSON.parse(data.target.result as string));
+      } else {
+        reject("no data");
+      }
+    };
+  });
+};
+
+export const transBlob2ArrayBuffer = (blob: Blob): Promise<ArrayBuffer> => {
+  return new Promise((resolve) => {
+    blob.arrayBuffer().then((buffer) => {
+      // ArrayBuffer
+      resolve(buffer);
+    });
+  });
+};
